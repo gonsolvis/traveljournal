5e7c0e1fc1cecdb0807f4823fb46fcc60ba2229c
gonsolvis <121898200+gonsolvis@users.noreply.github.com>
2023-04-03 17:52:57 +0200

    updated

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,11 +5,11 @@ import { BsGlobeAmericas } from 'react-icons/bs';
 function Nav() {
 
 return(
-<>
+
 <nav className="nav  ">
 <h1 className="nav--text"> <BsGlobeAmericas size={18}/>  my travel journal. </h1>    
 </nav>
-</>
+
 )}
 
 // 1x - 14px
